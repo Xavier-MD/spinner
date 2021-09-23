c1623875be8407e5153e4a77f3fed2af74ed3455
Xavier Martin Demers <xavier.pmd@outlook.com>
2021-09-23 02:17:59 -0400

    Refactored spinner1.js in the file spinner2.js so that it only uses 10 lines of code.

---
+++ spinner2.js
@@ -0,0 +1,10 @@
+let spinnerCharacters = ['|', '/', '-', '\\', '|', '/', '-', '\\', '|', '\n'];
+
+for (let x = 0; x < spinnerCharacters.length; x++) {
+  if (spinnerCharacters[x] !== '\n') {
+    spinnerCharacters[x] = `\r${spinnerCharacters[x]}   `;
+  }
+  setTimeout(() => {
+    process.stdout.write(spinnerCharacters[x]);
+  }, 100 + (x * 200));
+}
